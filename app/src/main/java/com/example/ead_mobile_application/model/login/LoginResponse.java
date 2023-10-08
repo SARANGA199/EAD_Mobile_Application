d1a7dd4b9093dbd78864b35ffc160da64a2a0549
Piumika Saranga <IT20260460@my.sliit.lk>
2023-10-09 01:37:44 +0530

    add login functionality

---
+++ app/src/main/java/com/example/ead_mobile_application/model/login/LoginResponse.java
@@ -0,0 +1,67 @@
+package com.example.ead_mobile_application.model.login;
+
+public class LoginResponse {
+    private String nic;
+    private String name;
+    private String email;
+    private String password;
+    private boolean is_active;
+    private int user_role;
+    private String errorMessage;
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getIsActive() {
+        return is_active;
+    }
+
+    public void setIsActive(boolean is_active) {
+        this.is_active = is_active;
+    }
+
+    public int getUser_role() {
+        return user_role;
+    }
+
+    public void setUser_role(int user_role) {
+        this.user_role = user_role;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
